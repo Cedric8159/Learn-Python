599b837a90c086f91da04faa0a7a21624afb2081
Cedric8159 <cedric.sebineza@gmail.com>
2024-12-13 23:29:02 -0600

    Add introduction for Day 1

---
+++ Day_1/helloworld.py
@@ -0,0 +1,30 @@
+# Introduction
+# Day 1 - Learning Python From Scratch
+
+# Arithmetic Operators
+print(3 + 2)   # addition(+)
+print(3 - 2)   # subtraction(-)
+print(3 * 2)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Shortcut Operators
+print("+=")  # Addition and assign
+print("-=")  # Subtraction and assign
+print("*=")  # Multiplication and assign
+print("/=")  # Division and assign
+print("%=")  # Modulus and assign
+print("**=") # Exponent and assign
+print("//=") # Floor Division and assign
+
+# Data Types
+print(type(12))                  # Int 
+print(type(0.90))                # Float 
+print(type('Pepsi'))             # String 
+print(type(['Orange', 'Apple'])) # List 
+print(type({'City':'London'}))   # Dictionary
+print(type({1, 2, 3, 4, 5}))     # Set
+print(type((34, 22, 15, 45)))    # Tuple
+print(type(True))                # Bool 
